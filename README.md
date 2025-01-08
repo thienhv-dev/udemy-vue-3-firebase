0242d88f621d4d1fbe742750c848a25d38d61901
MACBOOK_163 <macbook_163@MACBOOK-163s-MacBook-Pro.local>
2025-01-08 16:47:28 +0700

    [Section-3] The Vue CLI (for Bigger Projects)

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 Learn Vue JS 3 & Firebase by creating & deploying dynamic web apps (including Authentication).
 - [Build Web Apps with Vue JS 3 & Firebase](https://www.udemy.com/course/build-web-apps-with-vuejs-firebase/?couponCode=ST15MT100124B).
 - [Document vuejs](https://vuejs.org/guide/introduction.html)
+
+Setting NPM install
+- Node version: v18.17.0
+
+### About Service cmd
+
+```shell
+npm install -g @vue/cli
+```
---
+++ modal-project/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <h1>{{ title }}</h1>
+  <p>Welcome</p>
+  <teleport to="#modals" v-if="showModal">
+    <Modal theme="" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Sign up now</a>
+        <a href="#">More info</a>
+      </template>
+      <h1>Sign up for the Giveaway!</h1>
+      <p>Grab your Giveaway!</p>
+    </Modal>
+  </teleport>
+
+  <teleport to="#modals" v-if="showModalTwo">
+    <Modal theme="" @close="toggleModalTwo">
+      <h1>Sign up for the Giveaway!</h1>
+      <p>Grab your Giveaway!</p>
+    </Modal>
+  </teleport>
+  <button @click="toggleModal">open modal</button>
+  <button @click="toggleModalTwo">open modal two</button>
+</template>
+
+<script>
+import Modal from "@/components/Modal.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      title: 'Hello Vue.js App',
+      showModal: false,
+      showModalTwo: false
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    }
+  }
+}
+</script>
+
+<style>
+#app, #modals {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
