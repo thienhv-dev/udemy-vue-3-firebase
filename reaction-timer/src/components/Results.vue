23f09d89a5d01a096cb82de283bc45d813cb499a
MACBOOK_163 <macbook_163@MACBOOK-163s-MacBook-Pro.local>
2025-01-09 10:41:59 +0700

    [Section-4] PROJECT BUILD - Reaction Timer

---
+++ reaction-timer/src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction time: {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      rank: null
+    }
+  },
+  mounted() {
+    if (this.score.length < 250) {
+      this.rank = "Very Good"
+    } else if (this.score.length > 400) {
+      this.rank = "Bad"
+    } else {
+      this.rank = "Good"
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .rank {
+    font-size: 1.4em;
+    font-weight: bold;
+    color: #0faf87
+  }
+</style>
