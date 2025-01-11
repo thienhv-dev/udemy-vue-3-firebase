1b623ac85a2077c0001a9e64f6aebe5628f227b3
MACBOOK_163 <macbook_163@MACBOOK-163s-MacBook-Pro.local>
2025-01-11 11:39:19 +0700

    [Section-9] The composition API

---
+++ thienhv-blog/src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <input type="text" v-model="search">
+    <p>search term - {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+  </div>
+</template>
+
+<script>
+import { ref, computed } from "vue";
+
+export default {
+  name: 'HomeView',
+  setup() {
+    const search = ref('');
+    const names = ref(['thien', 'hieu', 'viet', 'hoa']);
+
+    const matchingNames = computed(() => {
+      return names.value.filter(name => name.includes(search.value));
+    });
+
+    return { names, search, matchingNames };
+  },
+}
+</script>
